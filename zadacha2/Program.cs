378a801974cdb9fd69fdcd32e5fce0505b4d3337
«Saro» <opac4321@mail.ru>
2022-12-07 14:28:33 +0300

    end zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int FindSum(int numberA)
+{
+    int sum = 0;
+
+    if (numberA > 0)
+    {
+
+        for (int remains = numberA; remains > 0; remains = remains / 10)
+        {
+            sum = sum + remains % 10;
+        }
+
+        Console.WriteLine($"сумма чисел = {sum}");
+    }
+
+    else // можно было обойтись без отрицательных значений дабы не усложнятьБ но в двнном варианте более универсальный метод получаеться
+    {
+
+        for (int remains = numberA; remains < 0; remains = remains / 10)
+        {
+            sum = sum + remains % 10;
+        }
+
+       Console.WriteLine($"сумма чисел = {sum}");
+    }
+    return sum;
+}
+
+Console.WriteLine("введите число number");
+
+int numberA = int.Parse(Console.ReadLine());
+
+int resalt = FindSum(numberA);
